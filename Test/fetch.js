5eb3d0b7404b0d2e3cde6ae93f5db4d419edd4ba
FeelTheMoment <42528722+FeelTheMoment@users.noreply.github.com>
2018-09-04 22:30:49 +0900

    Create fetch.js

---
+++ Test/fetch.js
@@ -0,0 +1,15 @@
+ fetch('List').then(function(response){
+    response.text().then(function(text){
+      var items = text.split(',');
+      var i = 0;
+      var tags = '';
+      while(i<items.length){
+        var item = items[i];
+        item = item.trim();
+        var tag = '<li><a href="#!'+item+'" onclick="fetchPage(\''+item+'\')">'+item+'</a></li>';
+        tags = tags + tag;
+        i = i + 1;
+      }
+      document.querySelector('#nav').innerHTML = tags;
+    })
+  });
